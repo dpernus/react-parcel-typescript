b32b08cee837f0f8079c20586128464ece1079b4
dpernus <dpernus@gmail.com>
2020-12-12 18:44:02 -0300

    Update header component to fix items space

--- src/components/Resumee/Header.tsx
+++ src/components/Resumee/Header.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 
-function formatKeyTerms(keyTerms: string[]): string {
-  const keyTermsFormatted = keyTerms.reduce((keyTerm1, keyTerm2) => keyTerm1 + '  |  ' + keyTerm2)
-  return keyTermsFormatted
+function formatKeyTerms(keyTerms: string[]) {
+  const keyTermsFormatted = keyTerms.reduce<JSX.Element[]>(
+    (acc, item) => [...acc, <span style={{ margin: '0 10px' }}>|</span>, <span>{item}</span>],
+    []
+  )
+
+  return <i className="secondary-text-color">{keyTermsFormatted.splice(1)}</i>
 }
 
 export const Header: React.FunctionComponent<{ name: string, keyTerms: string[] }> = ({ name, keyTerms }) => {
   return (
     <header id="head-content">
       <h1>{name}</h1>
-      <i className="secondary-text-color">{formatKeyTerms(keyTerms)}</i>
+      {formatKeyTerms(keyTerms)}
     </header>
   )
 }
