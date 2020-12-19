726fbab7637f3365c6a30c092cf6bc47ccc47e4e
dpernus <dpernus@gmail.com>
2020-12-19 10:20:48 -0300

    Update personal info component

--- src/components/Resumee/PersonalInfo.tsx
+++ src/components/Resumee/PersonalInfo.tsx
@@ -7,18 +7,20 @@ export interface PersonalData {
   address?: string
   phone?: number
   social?: string
+  git?: string
 }
 
 export const PersonalInfo: React.FunctionComponent<{ personalData: PersonalData }> = ({ personalData }) => {
-  const { age, email, phone, address, social } = personalData
+  const { age, email, phone, address, social, git } = personalData
   return (
     <div className="basic-info">
-      <div className="picture"></div>
+      {/* <div className="picture"></div> */}
       <div id="age" className="item"><i className="fa fa-user"></i><span>{age} años</span></div>
-      {address && <div id="address" className="item"><i className="fa fa-map-marker"></i><span></span></div>}
-      {phone && <div id="phone" className="item"><i className="fa fa-phone"></i><span></span></div>}
+      {address && <div id="address" className="item"><i className="fa fa-map-marker"></i><span>{address}</span></div>}
+      {phone && <div id="phone" className="item"><i className="fa fa-phone"></i><span>{phone}</span></div>}
       <div id="email" className="item"><i className="fa fa-envelope"></i><span>{email}</span></div>
-      {social && <div id="linkedin" className="item"><i className="fa fa-linkedin"></i><span></span></div>}
+      {social && <div id="linkedin" className="item"><i className="fa fa-linkedin"></i><a href={`https://${social}`} target="_blank">{social}</a></div>}
+      {git && <div id="git" className="item"><i className="fa fa-github"></i><a href={`https://${git}`} target="_blank">{git}</a></div>}
     </div>
   )
 }
